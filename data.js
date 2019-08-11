f0a28a5463819addeb0efda6787471994d5e5400
Korinne Northwood <kozimoto@gmail.com>
2019-08-11 19:00:41 +1000

    first

---
+++ data.js
@@ -0,0 +1,110 @@
+
+
+let oralNames = [
+  "Chlorpromazine",
+  "Flupentixol",
+  "Haloperidol",
+  "Levomepromazine",
+  "Pericyazine",
+  "Perphenazine",
+  "Pimozide",
+  "Sulpiride",
+  "Trifluoperizine",
+  "Zuclopenthixol",
+  "Amisulpride",
+  "Aripiprazole",
+  "Asenapine",
+  "Clozapine",
+  "Lurasidone HCL",
+  "Lurasidone Base",
+  "Olanzapine",
+  "Paliperidone",
+  "Quetiapine",
+  "Quetiapine MR",
+  "Risperidone",
+  "Sertindole",
+  "Brexipiprazole",
+  "Cariprazine",
+  "Iloperidone",
+  "Molidone",
+  "Ziprasidone"
+];
+
+
+let maxOralDosages = [
+  1000,
+  18,
+  20,
+  1000,
+  300,
+  24,
+  20,
+  2400,
+  30,
+  150,
+  1200,
+  30,
+  20,
+  900,
+  160,
+  148,
+  20,
+  12,
+  750,
+  800,
+  16,
+  24,
+  4,
+  6,
+  24,
+  225,
+  160
+];
+
+let depotNames = [
+  "Aripiprazole",
+  "Flupentixol",
+  "Fluphenazine",
+  "Haloperidol",
+  "Paliperidone",
+  "Paliperidone 3M",
+  "Pipotiazine",
+  "Risperidone",
+  "Zuclopenthixol",
+  "Olanzapine"
+];
+
+let maxDepotDosages = [
+  400,
+  400,
+  100,
+  300,
+  150,
+  525,
+  200,
+  50,
+  600,
+  300
+];
+
+let depotMaxTimings = [
+  4,
+  1,
+  2,
+  4,
+  4,
+  12,
+  4,
+  2,
+  1,
+  2
+];
+
+let depotTimingDivisor = [
+	1,
+  2,
+  4,
+  12
+];
+
+let timings = ["1 Week", "2 Weeks", "4 Weeks", "12 Weeks"];
---
+++ scripts.js
@@ -0,0 +1,188 @@
+
+var perscriptionSelection = document.createElement("select");
+
+var oralPerscriptionSelection = document.createElement("select");
+var oralDosageText = document.createElement("input");
+
+var oralTableDiv = document.querySelector("#oralTable");
+var depotTableDiv = document.querySelector("#depotTable");
+
+var oralTable = document.createElement("table");
+var depotTable = document.createElement("table");
+
+var timingsSelection = document.createElement("select");
+var dosageText = document.createElement("input");
+
+for (let i = 0; i < oralNames.length; ++i)
+{
+	let oral = oralNames[i];
+  oralPerscriptionSelection.options.add(new Option(oral));
+}
+
+for(let i = 0; i < depotNames.length; i++)
+{
+	let depot = depotNames[i];
+  perscriptionSelection.options.add(new Option(depot));
+}
+
+for (let i = 0; i<timings.length; ++i)
+{
+	let timing = timings[i];
+  timingsSelection.options.add(new Option(timing));
+}
+
+var addDepotBtn = document.createElement("button");
+addDepotBtn.appendChild(document.createTextNode("Add Depot"));
+addEvent(addDepotBtn,'click',addDepot);
+
+var addOralBtn = document.createElement("button");
+addOralBtn.appendChild(document.createTextNode("Add Oral"));
+addEvent(addOralBtn,'click',addOral);
+
+document.getElementById("oral_name").appendChild(oralPerscriptionSelection);
+document.getElementById("oral_dosage").appendChild(oralDosageText);
+document.getElementById("oral_add").appendChild(addOralBtn);
+
+document.getElementById("depot_name").appendChild(perscriptionSelection);
+document.getElementById("depot_timing").appendChild(timingsSelection);
+document.getElementById("depot_dosage").appendChild(dosageText);
+document.getElementById("depot_add").appendChild(addDepotBtn);
+
+depotTableDiv.appendChild(depotTable);
+oralTableDiv.appendChild(oralTable);
+
+var depots = [];
+var orals = [];
+var allPrescriptions = [];
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
+function addEvent(element, evnt, funct)
+{
+  if (element.attachEvent)
+   return element.attachEvent('on'+evnt, funct);
+  else
+   return element.addEventListener(evnt, funct, false);
+}
+
+function generateTableHead(table, data)
+{
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data)
+  {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function clearTable(table)
+{
+	while(table.hasChildNodes())
+  {
+     table.removeChild(table.firstChild);
+  }
+}
+
+function generateTable(table, data)
+{
+  for (let element of data)
+  {
+    let row = table.insertRow();
+    for (key in element)
+    {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
+
+function updateBnfTotals()
+{
+	let bnfTotalDepot = 0;
+  for (let i =0; i<depots.length; ++i)
+  {
+  	bnfTotalDepot += depots[i].bnf;
+	}
+
+  let bnfTotalOral = 0;
+  for (let i = 0; i<orals.length; ++i)
+  {
+  	bnfTotalOral += orals[i].bnf;
+	}
+
+  let total = bnfTotalDepot + bnfTotalOral;
+
+  document.getElementById("total_depot_bnf").innerHTML = roundToTwo(bnfTotalDepot);
+  document.getElementById("total_oral_bnf").innerHTML = roundToTwo(bnfTotalOral);
+  document.getElementById("total_bnf").innerHTML = roundToTwo(total);
+
+  clearTable(oralTable);
+  generateTableHead(oralTable, Object.keys(allPrescriptions[0]));
+  generateTable(oralTable, allPrescriptions);
+}
+
+function addOral()
+{
+	let dosage = parseInt(oralDosageText.value);
+  if (Number.isInteger(dosage) == false || dosage <= 0)
+  {
+  	alert("Invalid dosage");
+    return;
+	}
+
+  let index = oralPerscriptionSelection.selectedIndex;
+  let maxDosage = maxOralDosages[index];
+
+  let bnf = dosage / maxDosage;
+
+ 	let oralEntry = {
+	  "Perscription Name": oralNames[index] + " [oral]",
+    "Dosage (mg)": dosage,
+    Timing: "1 Day",
+    BNF: roundToTwo(bnf)
+	};
+
+  orals.push(oralEntry);
+  allPrescriptions.push(oralEntry);
+
+  updateBnfTotals();
+}
+
+function addDepot()
+{
+	let dosage = parseInt(dosageText.value);
+  if (Number.isInteger(dosage) == false || dosage <= 0)
+  {
+  	alert("Invalid dosage");
+    return;
+	}
+
+  let index = perscriptionSelection.selectedIndex;
+  let maxDosage = maxDepotDosages[index];
+
+  let maxTiming = depotMaxTimings[index] * 7;
+
+  let weeksIndex = timingsSelection.selectedIndex;
+  let weeks = depotTimingDivisor[weeksIndex] ;
+  let days = weeks * 7;
+
+  let bnf = (dosage / (days/maxTiming)) / maxDosage;
+
+ 	let depotEntry = {
+		"Perscription Name": depotNames[index] + " [depot]",
+    "Dosage (mg)": dosage,
+    Timing: timings[weeksIndex],
+    BNF: roundToTwo(bnf)
+	};
+
+  depots.push(depotEntry);
+  allPrescriptions.push(depotEntry);
+
+  updateBnfTotals();
+}

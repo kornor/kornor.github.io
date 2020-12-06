49e65dbb909e94a0036ad595833ba4f6b4110b35
kornor <korinnen@gmail.com>
2020-12-06 13:15:34 +1000

    Create data_swapp.js
    Updated data table with AMH dosings

---
+++ data_swapp.js
@@ -0,0 +1,122 @@
+
+
+let oralNames = [
+ "Amisulpride",
+"Aripiprazole",
+"Asenapine",
+"Brexipiprazole",
+"Cariprazine",
+"Chlorpromazine",
+"Clozapine",
+"Flupentixol",
+"Haloperidol",
+"Iloperidone",
+"Levomepromazine",
+"Lurasidone Base",
+"Lurasidone HCL",
+"Molidone",
+"Olanzapine",
+"Paliperidone",
+"Pericyazine",
+"Perphenazine",
+"Pimozide",
+"Quetiapine",
+"Quetiapine MR",
+"Risperidone",
+"Sertindole",
+"Sulpiride",
+"Trifluoperizine",
+"Ziprasidone",
+"Zuclopenthixol"
+];
+
+
+let maxOralDosages = [
+ 1200,
+30,
+20,
+4,
+6,
+600,
+900,
+18,
+10,
+24,
+1000,
+148,
+160,
+225,
+20,
+12,
+75,
+24,
+20,
+750,
+800,
+6,
+24,
+2400,
+30,
+160,
+75,
+];
+
+let depotNames = [
+  "Aripiprazole",
+  "Flupentixol",
+  "Fluphenazine",
+  "Haloperidol",
+  "Olanzapine",
+  "Paliperidone (Sustenna)",
+  "Paliperidone (Trinza)",
+  "Pipotiazine",
+  "Risperidone",
+  "Zuclopenthixol",
+
+];
+
+let maxDepotDosages = [
+  400,
+  50,
+  100,
+  300,
+  300,
+  150,
+  525,
+  200,
+  50,
+  200,
+
+];
+
+let depotMaxTimings = [
+  4,
+  1,
+  2,
+  4,
+  2,
+  4,
+  12,
+  4,
+  2,
+  1,
+];
+
+let depotTimingDivisor = [
+	1,
+  2,
+  3,
+  4,
+  12
+];
+
+let oralTimingsMultiplier = [
+  1,
+  2,
+  3,
+  4,
+];
+
+let timings = ["1 Week", "2 Weeks", "3 Weeks", "4 Weeks", "12 Weeks"];
+
+let oralTimings = ["Once daily", "Twice daily", "Three times daily", "Four times daily"];
